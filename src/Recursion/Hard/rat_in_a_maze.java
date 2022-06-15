1ce6af15768f1e00a9de4995166d11c70b5e8eea
Nishchay <nishchaysingh331@gmail.com>
2022-06-15 20:25:17 +0530

    commit 5

--- src/Recursion/Hard/rat_in_a_maze.java
+++ src/Recursion/Hard/rat_in_a_maze.java
@@ -17,11 +17,22 @@ public class rat_in_a_maze {
 
 
         String dir = "DLRU";
+
+        //this loop is for the index for di[] and dj[] and dir string
         for (int ind = 0; ind < 4; ind++) {
             int nexti = i + di[ind];
             int nextj = j + dj[ind];
-            if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n &&
-                    vis[nexti][nextj] == 0 && a[nexti][nextj] == 1) {
+
+            //nexti >= 0 && nextj >= 0 && nexti < n && nextj < n --> nexti and nextj should be should be greater than or = to 0 and
+            //less than n than only we can move
+            //a[nexti][nextj] == 1 -->only move if there is 1 in a[][] maze
+            //vis[nexti][nextj] == 0 --> it should be 0 (mean not visited) , should not be 1 (will mean that we have visited ), path should be unique
+
+            if (nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && vis[nexti][nextj] == 0 && a[nexti][nextj] == 1)
+
+
+
+            {
 
                 vis[i][j] = 1;
                 solve(nexti, nextj, a, n, ans, move + dir.charAt(ind), vis, di, dj);
@@ -64,7 +75,7 @@ public class rat_in_a_maze {
 
         if (res.size() > 0) {
             for (int i = 0; i < res.size(); i++)
-                System.out.print(res.get(i) + " ");
+                System.out.print(res.get(i) +" ");
             System.out.println();
         } else {
             System.out.println(-1);
