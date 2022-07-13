c288e9d1bee53ef382078e70e0969b520648397a
Nishchay <nishchaysingh331@gmail.com>
2022-07-13 18:54:32 +0530

    commit 23

---
+++ src/Trees/maxHeightOfBT.java
@@ -0,0 +1,36 @@
+package Trees;
+
+//TC = O(N)
+//SC = O(N) = O(Height of tree)
+public class maxHeightOfBT {
+
+    static class Node{
+        int data;
+        Node left,right;
+
+        Node(int key){
+            data = key;
+            left = right = null;
+        }
+    }
+
+    static int maxHeightOfBT(Node root){
+        //at any node find the height of left tree , then height of right tree and calculate the max of both the heights
+
+        if (root == null) return 0;
+
+        int lh = maxHeightOfBT(root.left);
+        int rh = maxHeightOfBT(root.right);
+        return 1+ Math.max(lh,rh);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(3);
+        root.left = new Node(9);
+        root.right = new Node(20);
+        root.right.left = new Node(15);
+        root.right.right = new Node(7);
+
+
+    }
+}
