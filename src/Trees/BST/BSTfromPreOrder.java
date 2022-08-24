6ff8fdeab10f75191b35c5c45eb279806ec33127
Nishchay <nishchaysingh331@gmail.com>
2022-08-24 18:15:47 +0530

    commit 53

---
+++ src/Trees/BST/BSTfromPreOrder.java
@@ -0,0 +1,38 @@
+package Trees.BST;
+
+//TC = O(N)
+//SC = O(1) -> Not considering auxiliary stack space
+
+public class BSTfromPreOrder {
+
+    static class Node{
+        int data;
+        Node left , right;
+
+        Node(int key){
+            data = key;
+            left = right = null;
+        }
+    }
+
+    private static Node bstfromPreOrder(int[] A){
+        return bstfromPreOrder(A,Integer.MAX_VALUE , new int[]{0});
+    }
+
+    private static Node bstfromPreOrder(int[] A,int bound , int[] i){
+        if(i[0] == A.length || A[i[0]] > bound) return null;
+
+        Node root = new Node(A[i[0]++]);
+        root.left = bstfromPreOrder(A , root.data , i);
+        root.right = bstfromPreOrder(A , bound , i);
+        return root;
+    }
+
+    public static void main(String[] args) {
+       int preorder[] = {8,5,1,7,10,12};
+
+       Node root = bstfromPreOrder(preorder);
+       System.out.println(root.data);
+
+    }
+}
